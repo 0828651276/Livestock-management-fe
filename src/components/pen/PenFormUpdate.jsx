7be65dce9c477d43b2c3bb10ae3a89f9a65e5647
LeVanChuyen-20 <20222411@eaut.edu.vn>
2025-04-28 13:50:40 +0700

    fixed pen

--- src/components/pen/PenFormUpdate.jsx
+++ src/components/pen/PenFormUpdate.jsx
@@ -282,19 +282,19 @@ const PigPenFormUpdate = ({ onClose, pigPenData }) => {
                     className={errors.closedDate ? "field-error" : ""}
                 />
 
-                <TextField
-                    label="Số lượng"
-                    name="quantity"
-                    type="number"
-                    value={pigPen.quantity}
-                    onChange={handleChange}
-                    required
-                    error={!!errors.quantity}
-                    helperText={errors.quantity}
-                    InputProps={{ inputProps: { min: 0 } }}
-                    sx={{ "& .MuiInputBase-input": { py: 1.5 } }}
-                    className={errors.quantity ? "field-error" : ""}
-                />
+                {/*<TextField*/}
+                {/*    label="Số lượng"*/}
+                {/*    name="quantity"*/}
+                {/*    type="number"*/}
+                {/*    value={pigPen.quantity}*/}
+                {/*    onChange={handleChange}*/}
+                {/*    required*/}
+                {/*    error={!!errors.quantity}*/}
+                {/*    helperText={errors.quantity}*/}
+                {/*    InputProps={{ inputProps: { min: 0 } }}*/}
+                {/*    sx={{ "& .MuiInputBase-input": { py: 1.5 } }}*/}
+                {/*    className={errors.quantity ? "field-error" : ""}*/}
+                {/*/>*/}
 
                 <Box sx={{ mb: 2 }}>
                     <FormControl fullWidth>
