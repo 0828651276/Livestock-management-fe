1e149db20f805358520dd5dec7466a5493cc143c
hoangnhat4280 <nhatduong3002@gmail.com>
2025-05-27 08:56:36 +0700

    update noti2

--- src/services/NotificationService.js
+++ src/services/NotificationService.js
@@ -38,6 +38,24 @@ export const notificationService = {
         return res.data;
     },
 
+    // Cập nhật thông báo
+    update: async (id, notification) => {
+        const token = authService.getCurrentUser();
+
+        // Đặt trạng thái read về false để reset thông báo thành "chưa đọc"
+        const updatedNotification = {
+            ...notification,
+            read: false
+        };
+
+        const res = await axios.put(`${API_URL}/${id}`, updatedNotification, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            },
+        });
+        return res.data;
+    },
 
     // Xóa thông báo
     delete: async (id) => {
